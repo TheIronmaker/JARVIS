e24f590c6775735ad666e8ab72343172933b0dab
TheIronmaker <a.weston.morris@gmail.com>
2026-03-08 17:52:24 -0700

    View Beginning

--- src/jarvis/app_core/views/atom_sim/view.py
+++ src/jarvis/app_core/views/atom_sim/view.py
@@ -1,4 +1,5 @@
-from PySide6.QtWidgets import QWidget
+from PySide6.QtWidgets import QVBoxLayout, QWidget, QLabel
+from PySide6.QtCore import Qt
 
 class AtomSimView(QWidget):
     def __init__(self, name, parent):
@@ -8,3 +9,13 @@ class AtomSimView(QWidget):
         
         self.bus_global = parent.bus
         self.bus = parent.bus.namespaced(name)
+
+        self.layout = QVBoxLayout()
+        self.layout.setContentsMargins(0, 0, 0, 0)
+
+        self.content_box = QLabel("AtomSim View")
+        self.content_box.setStyleSheet("background-color: black; color: white;")
+        self.content_box.setAlignment(Qt.AlignCenter)
+        self.layout.addWidget(self.content_box)
+
+        self.setLayout(self.layout)
